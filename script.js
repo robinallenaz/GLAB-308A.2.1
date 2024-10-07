1796589cb96d6afc34c43fcedb6fda466fb60409
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-10-06 19:28:43 -0700

    Created a class named Character with properties name, health, and inventory

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ adventurer.companion = {
     console.log(`${this.name} rolled a ${result}.`);
  };
  
+ class Character {
+    constructor(name) {
+       this.name = name;
+       this.health = 100;
+       this.inventory = [];
+    }
+    roll(mod = 0) {
+       const result = Math.floor(Math.random() * 20) + 1 + mod;
+       console.log(`${this.name} rolled a ${result}.`);
+    }
+ }
+ 
